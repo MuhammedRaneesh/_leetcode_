3524746446b61896fe62de40e0abb7e64cec7c13
Muhammed Raneesh <raneeshramankulam@gmail.com>
2026-07-19 22:21:10 +0530

    Add solution for Single Number

--- LeetCode/Single-Number/Solution.js
+++ LeetCode/Single-Number/Solution.js
@@ -3,19 +3,26 @@
 3 * @return {number}
 4 */
 5var singleNumber = function(nums) {
-6    let value = {}
+6    // let value = {}
 7
-8    for(let i = 0; i < nums.length ; i++){
-9        if(value[nums[i]]){
-10           value[nums[i]] = value[nums[i]] + 1 
-11        }else {
-12            value[nums[i]] = 1 
-13        }
-14    }
+8    // for(let i = 0; i < nums.length ; i++){
+9    //     if(value[nums[i]]){
+10    //        value[nums[i]] = value[nums[i]] + 1 
+11    //     }else {
+12    //         value[nums[i]] = 1 
+13    //     }
+14    // }
 15
-16    for(let key in value){
-17        if(value[key] === 1){
-18            return Number(key)
-19        }
-20    }
-21};
+16    // for(let key in value){
+17    //     if(value[key] === 1){
+18    //         return Number(key)
+19    //     }
+20    // }
+21
+22    let result = 0;
+23    for (let i = 0; i < nums.length; i++) {
+24        result ^= nums[i];
+25    }
+26    return result;
+27
+28};
