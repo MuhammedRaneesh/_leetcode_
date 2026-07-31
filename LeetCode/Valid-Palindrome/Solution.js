2803ecc207ee2aaf9d44847e7dae2bdb49b4cd37
Muhammed Raneesh <raneeshramankulam@gmail.com>
2026-07-31 23:24:51 +0530

    Add solution for Valid Palindrome

--- LeetCode/Valid-Palindrome/Solution.js
+++ LeetCode/Valid-Palindrome/Solution.js
@@ -4,17 +4,16 @@
 4 */
 5var isPalindrome = function(s) {
 6    let word = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
-7    console.log(word)
-8    let left = 0 ;
-9    let right = word.length - 1
-10    
-11    while(left < right){
-12        if(word[left]!== word[right]){
-13            return false
-14        }
-15       
-16        left++
-17        right--
-18    }
-19    return true 
-20};
+7    let left = 0 ;
+8    let right = word.length - 1
+9
+10    while(left < right){
+11        if(word[left]!== word[right]){
+12            return false
+13        }
+14       
+15        left++
+16        right--
+17    }
+18    return true 
+19};
