51e5fc1fdb7e8586c96872cddce53e70864eb410
Muhammed Raneesh <raneeshramankulam@gmail.com>
2026-08-03 17:02:09 +0530

    Add solution for Restore Finishing Order

--- LeetCode/Restore-Finishing-Order/Solution.js
+++ LeetCode/Restore-Finishing-Order/Solution.js
@@ -4,15 +4,26 @@
 4 * @return {number[]}
 5 */
 6var recoverOrder = function(order, friends) {
-7    let final = []
-8    for(let i = 0 ; i < order.length ; i++){
+7    // let final = []
+8    // for(let i = 0 ; i < order.length ; i++){
 9
-10        for(let j = 0 ; j < friends.length ; j++){
-11            if(order[i] === friends[j]){
-12                final.push(order[i])
-13                break;
-14            }
-15        }
-16    }
-17    return final
-18};
+10    //     for(let j = 0 ; j < friends.length ; j++){
+11    //         if(order[i] === friends[j]){
+12    //             final.push(order[i])
+13    //             break;
+14    //         }
+15    //     }
+16    // }
+17    // return final
+18
+19    let final = []
+20
+21    let friend = new Set(friends)
+22
+23    for(let i = 0 ; i < order.length ; i++){
+24        if(friend.has(order[i])){
+25            final.push(order[i]);
+26        }
+27    }
+28    return final
+29};
