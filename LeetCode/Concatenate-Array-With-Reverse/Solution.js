0ed219ebf61b57d885c3c1123229283a92163179
Muhammed Raneesh <raneeshramankulam@gmail.com>
2026-08-03 15:58:24 +0530

    Add solution for Concatenate Array With Reverse

--- LeetCode/Concatenate-Array-With-Reverse/Solution.js
+++ LeetCode/Concatenate-Array-With-Reverse/Solution.js
@@ -4,10 +4,10 @@
 4 */
 5var concatWithReverse = function(nums) {
 6    let ans = []
-7    let n = nums.length
-8    for(let i = 0 ; i < n ; i++){
+7    
+8    for(let i = 0 ; i < nums.length ; i++){
 9        ans[i] = nums[i]
-10        ans[i + n ] = nums[ n - i - 1]
+10        ans[i + nums.length ] = nums[nums.length- i - 1]
 11    }
 12    return ans
 13};
